a0c667d4a62ea9e8366cfcecacd075b304117884
Jon Godbhaal <godbhaal666@gmail.com>
2018-03-08 11:13:08 +0100

    feat: Add both implementations of mnist

--- mnist.py
+++ mnist.py
@@ -1,3 +1,5 @@
+""" Implementation following TF doc """
+
 import tensorflow as tf
 
 import utils_mnist
@@ -6,15 +8,12 @@ import utils_mnist
 def basic_model_fn(features, labels, mode):
     """ Model function """
     # N samples of D values and H neurons per hidden layer
-    # N = 64  # samples
     D = 32 * 32  # sample size
     C = 10  # classes
     H = 256  # units in hidden layers
 
     # Input layer
     input_layer = tf.reshape(features['x'], [-1, D])
-    # input_data = tf.placeholder(tf.float32, shape=(None, D))
-    # input_labels = tf.placeholder(tf.float32, shape=(None, C))
 
     # Dense layers
     init = tf.contrib.layers.xavier_initializer()
@@ -41,7 +40,6 @@ def basic_model_fn(features, labels, mode):
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
     # Calculate loss
-    # loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
     one_hot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32),
                                 depth=C)
     loss = tf.losses.softmax_cross_entropy(onehot_labels=one_hot_labels,
@@ -51,8 +49,6 @@ def basic_model_fn(features, labels, mode):
 
     # Config training
     if mode == tf.estimator.ModeKeys.TRAIN:
-        # learning_rate = 1e-5
-        # optimizer = tf.train.GradientDescentOptimizer(learning_rate)
         optimizer = tf.train.AdamOptimizer()
         train_op = optimizer.minimize(loss=loss,
                                       global_step=tf.train.get_global_step())
@@ -71,16 +67,15 @@ def basic_model_fn(features, labels, mode):
                                       eval_metric_ops=eval_metric_ops)
 
 
-def main(data, batch_size=100, num_epochs=None, steps=None):
-    """
+def train_and_evaluate(data, batch_size=100, num_epochs=None, steps=None):
+    """ Trains and evaluates the model
+
     Args:
         data: mnist object
         batch_size: number of images to do one gradient update
         num_epochs: number of epochs to run, if `None` a number of steps are
             computed to run the equivalent to one epoch
         steps: number of steps to run,
-    Returns:
-
     """
     if (num_epochs is None and steps is None) or \
         (num_epochs is not None and steps is not None):
@@ -96,10 +91,6 @@ def main(data, batch_size=100, num_epochs=None, steps=None):
     mnist_classifier = tf.estimator.Estimator(model_fn=basic_model_fn,
                                               model_dir='tmp/mnist_basic_model')
 
-    tensors_to_log = {'probabilities': 'softmax_tensor'}
-    logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log,
-                                              every_n_iter=50)
-
     # Train the model
     if num_epochs is None:
         train_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -109,8 +100,7 @@ def main(data, batch_size=100, num_epochs=None, steps=None):
             shuffle=True)
 
         mnist_classifier.train(input_fn=train_input_fn,
-                               steps=steps,
-                               hooks=[])
+                               steps=steps)
 
     else:
         train_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -129,12 +119,12 @@ def main(data, batch_size=100, num_epochs=None, steps=None):
         num_epochs=1,
         shuffle=False)
 
-    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
-    print(eval_results)
+    return mnist_classifier.evaluate(input_fn=eval_input_fn)
 
 
 if __name__ == '__main__':
-    main(data=utils_mnist.load_mnist_32x32(),
-         batch_size=550,
-         num_epochs=10,
-         steps=None)
+    eval_results = train_and_evaluate(data=utils_mnist.load_mnist_32x32(),
+                                      batch_size=550,
+                                      num_epochs=10,
+                                      steps=None)
+    print(eval_results)
