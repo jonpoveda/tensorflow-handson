8dd90962e3a9664da0cf5a5bec60a1f16bb4379a
Jon Godbhaal <godbhaal666@gmail.com>
2018-03-08 17:39:57 +0100

    feat: Implement a base model class
    Reimplement mnist module as a OO implementation.

--- mnist.py
+++ mnist.py
@@ -5,126 +5,148 @@ import tensorflow as tf
 import utils_mnist
 
 
-def basic_model_fn(features, labels, mode):
-    """ Model function """
-    # N samples of D values and H neurons per hidden layer
-    D = 32 * 32  # sample size
-    C = 10  # classes
-    H = 256  # units in hidden layers
+class BaseModel(object):
+    def __init__(self):
+        pass
 
-    # Input layer
-    input_layer = tf.reshape(features['x'], [-1, D])
+    def model_function(self, features, labels, mode):
+        pass
 
-    # Dense layers
-    init = tf.contrib.layers.xavier_initializer()
-    hidden = tf.layers.dense(inputs=input_layer,
-                             units=H,
-                             activation=tf.nn.relu,
-                             kernel_initializer=init)
+    def train_and_evaluate(self, data,
+                           batch_size=100,
+                           num_epochs=None,
+                           steps=None):
+        pass
 
-    hidden2 = tf.layers.dense(inputs=hidden,
-                              units=H,
-                              activation=tf.nn.relu,
-                              kernel_initializer=init)
 
-    # Logits Layer
-    logits = tf.layers.dense(inputs=hidden2,
-                             units=C)
+class BasicModel(BaseModel):
+    def __init__(self):
+        super(BasicModel, self).__init__()
 
-    predictions = {
-        'classes': tf.argmax(input=logits, axis=1, name='classes'),
-        'probabilities': tf.nn.softmax(logits, name='softmax_tensor')
-    }
+    def model_function(self, features, labels, mode):
+        """ Model function """
+        # N samples of D values and H neurons per hidden layer
+        D = 32 * 32  # sample size
+        C = 10  # classes
+        H = 256  # units in hidden layers
 
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+        # Input layer
+        input_layer = tf.reshape(features['x'], [-1, D])
 
-    # Calculate loss
-    one_hot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32),
-                                depth=C)
-    loss = tf.losses.softmax_cross_entropy(onehot_labels=one_hot_labels,
-                                           logits=logits)
-    # loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels,
-    #                                        logits=logits)
+        # Dense layers
+        init = tf.contrib.layers.xavier_initializer()
+        hidden = tf.layers.dense(inputs=input_layer,
+                                 units=H,
+                                 activation=tf.nn.relu,
+                                 kernel_initializer=init)
 
-    # Config training
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.AdamOptimizer()
-        train_op = optimizer.minimize(loss=loss,
-                                      global_step=tf.train.get_global_step())
+        hidden2 = tf.layers.dense(inputs=hidden,
+                                  units=H,
+                                  activation=tf.nn.relu,
+                                  kernel_initializer=init)
 
+        # Logits Layer
+        logits = tf.layers.dense(inputs=hidden2,
+                                 units=C)
+
+        predictions = {
+            'classes': tf.argmax(input=logits, axis=1, name='classes'),
+            'probabilities': tf.nn.softmax(logits, name='softmax_tensor')
+        }
+
+        if mode == tf.estimator.ModeKeys.PREDICT:
+            return tf.estimator.EstimatorSpec(mode=mode,
+                                              predictions=predictions)
+
+        # Calculate loss
+        one_hot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32),
+                                    depth=C)
+        loss = tf.losses.softmax_cross_entropy(onehot_labels=one_hot_labels,
+                                               logits=logits)
+        # loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels,
+        #                                        logits=logits)
+
+        # Config training
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            optimizer = tf.train.AdamOptimizer()
+            train_op = optimizer.minimize(loss=loss,
+                                          global_step=tf.train.get_global_step())
+
+            return tf.estimator.EstimatorSpec(mode=mode,
+                                              loss=loss,
+                                              train_op=train_op)
+
+        # Add evaluation metrics
+        eval_metric_ops = {
+            'accuracy': tf.metrics.accuracy(labels=labels,
+                                            predictions=predictions['classes'])
+        }
         return tf.estimator.EstimatorSpec(mode=mode,
                                           loss=loss,
-                                          train_op=train_op)
+                                          eval_metric_ops=eval_metric_ops)
 
-    # Add evaluation metrics
-    eval_metric_ops = {
-        'accuracy': tf.metrics.accuracy(labels=labels,
-                                        predictions=predictions['classes'])
-    }
-    return tf.estimator.EstimatorSpec(mode=mode,
-                                      loss=loss,
-                                      eval_metric_ops=eval_metric_ops)
+    def train_and_evaluate(self, data,
+                           batch_size=100, num_epochs=None, steps=None):
+        """ Trains and evaluates the model
 
+        Args:
+            data: mnist object
+            batch_size: number of images to do one gradient update
+            num_epochs: number of epochs to run, if `None` a number of steps are
+                computed to run the equivalent to one epoch
+            steps: number of steps to run,
+        """
+        if (num_epochs is None and steps is None) or \
+            (num_epochs is not None and steps is not None):
+            raise ValueError('Please set one of num_epochs or steps. '
+                             'They are mutually exclusive')
 
-def train_and_evaluate(data, batch_size=100, num_epochs=None, steps=None):
-    """ Trains and evaluates the model
+        if num_epochs is None:
+            steps = data.train.images.shape[0] // batch_size + 1
 
-    Args:
-        data: mnist object
-        batch_size: number of images to do one gradient update
-        num_epochs: number of epochs to run, if `None` a number of steps are
-            computed to run the equivalent to one epoch
-        steps: number of steps to run,
-    """
-    if (num_epochs is None and steps is None) or \
-        (num_epochs is not None and steps is not None):
-        raise ValueError('Please set one of num_epochs or steps. '
-                         'They are mutually exclusive')
+        tf.logging.set_verbosity(tf.logging.INFO)
 
-    if num_epochs is None:
-        steps = data.train.images.shape[0] // batch_size + 1
+        # Create estimator
+        mnist_classifier = tf.estimator.Estimator(model_fn=self.model_function,
+                                                  model_dir='tmp/mnist_basic_model')
 
-    tf.logging.set_verbosity(tf.logging.INFO)
+        # Train the model
+        if num_epochs is None:
+            train_input_fn = tf.estimator.inputs.numpy_input_fn(
+                x={'x': data.train.images},
+                y=data.train.labels,
+                batch_size=batch_size,
+                shuffle=True)
 
-    # Create estimator
-    mnist_classifier = tf.estimator.Estimator(model_fn=basic_model_fn,
-                                              model_dir='tmp/mnist_basic_model')
+            mnist_classifier.train(input_fn=train_input_fn,
+                                   steps=steps)
 
-    # Train the model
-    if num_epochs is None:
-        train_input_fn = tf.estimator.inputs.numpy_input_fn(
-            x={'x': data.train.images},
-            y=data.train.labels,
-            batch_size=batch_size,
-            shuffle=True)
+        else:
+            train_input_fn = tf.estimator.inputs.numpy_input_fn(
+                x={'x': data.train.images},
+                y=data.train.labels,
+                batch_size=batch_size,
+                num_epochs=num_epochs,
+                shuffle=True)
 
-        mnist_classifier.train(input_fn=train_input_fn,
-                               steps=steps)
+            mnist_classifier.train(input_fn=train_input_fn)
 
-    else:
-        train_input_fn = tf.estimator.inputs.numpy_input_fn(
-            x={'x': data.train.images},
-            y=data.train.labels,
-            batch_size=batch_size,
-            num_epochs=num_epochs,
-            shuffle=True)
+        # Evaluate model
+        eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x={'x': data.test.images},
+            y=data.test.labels,
+            num_epochs=1,
+            shuffle=False)
 
-        mnist_classifier.train(input_fn=train_input_fn)
-
-    # Evaluate model
-    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={'x': data.test.images},
-        y=data.test.labels,
-        num_epochs=1,
-        shuffle=False)
-
-    return mnist_classifier.evaluate(input_fn=eval_input_fn)
+        return mnist_classifier.evaluate(input_fn=eval_input_fn)
 
 
 if __name__ == '__main__':
-    eval_results = train_and_evaluate(data=utils_mnist.load_mnist_32x32(),
-                                      batch_size=550,
-                                      num_epochs=10,
-                                      steps=None)
+    dataset = utils_mnist.load_mnist_32x32()
+    model = BasicModel()
+
+    eval_results = model.train_and_evaluate(data=dataset,
+                                            batch_size=550,
+                                            num_epochs=2,
+                                            steps=None)
     print(eval_results)
