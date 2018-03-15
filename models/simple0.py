041bf8455e8c6d95dc56cc80b036e3bcad9c25d8
Jon Godbhaal <godbhaal666@gmail.com>
2018-03-15 12:36:52 +0100

    feat: Implement tensorboard

--- models/simple0.py
+++ models/simple0.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """ Implementation of a simple neural network without TF wrappers """
+
 import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
@@ -22,50 +23,67 @@ class Simple0(BaseModel):
         C = 10  # classes
         H = 256  # units in hidden layers
 
-        input_data = tf.placeholder(tf.float32, shape=(None, D))
-        input_labels = tf.placeholder(tf.int32, shape=(None,))
+        with tf.name_scope('input'):
+            input_data = tf.placeholder(tf.float32, shape=(None, D))
+            input_labels = tf.placeholder(tf.int32, shape=(None,))
 
-        input_labels_one_hot = tf.one_hot(
-            indices=tf.cast(input_labels, tf.int32), depth=C, dtype=tf.int32)
+            input_labels_one_hot = tf.one_hot(
+                indices=tf.cast(input_labels, tf.int32), depth=C,
+                dtype=tf.int32)
 
-        init = tf.contrib.layers.xavier_initializer()
+        with tf.name_scope('hidden'):
+            init = tf.contrib.layers.xavier_initializer()
 
-        hidden = tf.layers.dense(inputs=input_data,
-                                 units=H,
-                                 activation=tf.nn.relu,
-                                 kernel_initializer=init)
+            hidden = tf.layers.dense(inputs=input_data,
+                                     units=H,
+                                     activation=tf.nn.relu,
+                                     kernel_initializer=init)
 
-        hidden2 = tf.layers.dense(inputs=hidden,
-                                  units=H,
-                                  activation=tf.nn.relu,
-                                  kernel_initializer=init)
+            hidden2 = tf.layers.dense(inputs=hidden,
+                                      units=H,
+                                      activation=tf.nn.relu,
+                                      kernel_initializer=init)
 
-        logits = tf.layers.dense(inputs=hidden2,
-                                 units=C,
-                                 kernel_initializer=init)
+        with tf.name_scope('predictions'):
+            logits = tf.layers.dense(inputs=hidden2,
+                                     units=C,
+                                     kernel_initializer=init)
 
-        loss = tf.losses.softmax_cross_entropy(
-            onehot_labels=input_labels_one_hot,
-            logits=logits)
+        with tf.name_scope('train_metrics'):
+            loss = tf.losses.softmax_cross_entropy(
+                onehot_labels=input_labels_one_hot,
+                logits=logits)
 
         # Update on training
         update_operation = tf.train.AdamOptimizer().minimize(loss)
 
-        predictions = {
-            "classes": tf.argmax(input=logits, axis=1, name='classes'),
-            "probabilities": tf.nn.softmax(logits, name='softmax_tensor')
-        }
+        with tf.name_scope('test_metrics'):
+            predictions = {
+                "classes": tf.argmax(input=logits, axis=1, name='classes'),
+                "probabilities": tf.nn.softmax(logits, name='softmax_tensor')
+            }
 
-        accuracy, accuracy_update_op = tf.metrics.accuracy(
-            labels=input_labels,
-            predictions=predictions['classes'],
-            name='accuracy')
+            accuracy, accuracy_update_op = tf.metrics.accuracy(
+                labels=input_labels,
+                predictions=predictions['classes'],
+                name='accuracy')
 
         # Update running vars behind the scene for accuracy
         running_vars = \
-            tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope='accuracy')
+            tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES,
+                              scope='test_metrics/accuracy')
         running_vars_initializer = \
             tf.variables_initializer(var_list=running_vars)
+
+        # Log metrics
+        summary_op_train = tf.summary.scalar('loss', loss)
+        summary_op_test = tf.summary.scalar('accuracy', accuracy)
+        summary_op = tf.summary.merge_all()
+
+        # Logging writer
+        writer = tf.summary.FileWriter(
+            f'tmp/{self.__class__.__name__}/tensorboard',
+            graph=tf.get_default_graph())
 
         with tf.Session() as sess:
             mnist = data
@@ -91,6 +109,7 @@ class Simple0(BaseModel):
                     n_batches = samples.shape[0] // batch_size
 
                 loss_function = []
+                i = 0
                 for epoch in range(epochs):
                     for batch_num in range(n_batches):
                         batch_data: np.ndarray = \
@@ -107,11 +126,14 @@ class Simple0(BaseModel):
                         values = {input_data: batch_data,
                                   input_labels: batch_labels}
 
-                        metrics = sess.run([update_operation, loss, ],
-                                           feed_dict=values)
-                        loss_function.append(metrics[1])
+                        _, metric, summary = sess.run(
+                            [update_operation, loss, summary_op_train],
+                            feed_dict=values)
+                        writer.add_summary(summary, i)
+                        i += 1
+                        loss_function.append(metric)
 
-                    print(f'Epoch: {epoch}, loss: {metrics[1]}')
+                    print(f'Epoch: {epoch}, loss: {metric}')
 
                 return loss_function
 
@@ -133,6 +155,7 @@ class Simple0(BaseModel):
                 if n_samples == -1:
                     n_batches = samples.shape[0] // batch_size
 
+                i = 0
                 for batch_num in range(n_batches):
                     batch_data: np.ndarray = samples[batch_num * batch_size:
                                                      (
@@ -148,16 +171,20 @@ class Simple0(BaseModel):
                     values = {input_data: batch_data,
                               input_labels: batch_labels}
 
-                    metrics = sess.run([accuracy_update_op, accuracy],
-                                       feed_dict=values)
+                    _, metric, summary = sess.run([accuracy_update_op,
+                                                   accuracy,
+                                                   summary_op_test],
+                                                  feed_dict=values)
+                    writer.add_summary(summary, i)
+                    i += 1
 
-                print(f'Final accuracy: {metrics[1]}')
-                return metrics[1]
+                print(f'Final accuracy: {metric}')
+                return metric
 
             train_labels = mnist.train.labels
             loss_function = train(samples=mnist.train.images,
                                   labels=train_labels,
-                                  n_samples=batch_size * 3,
+                                  n_samples=-1,
                                   batch_size=batch_size,
                                   epochs=num_epochs)
             plt.figure('Training loss')
